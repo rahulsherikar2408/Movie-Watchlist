47a5c44c0a51cb3fa9fec97db54cf005ef2b8578
Sherikar Rahul <sherikarrahul59@gmail.com>
2025-07-25 21:15:38 +0530

    Updating Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Banner from "./components/Banner";
+import Movies from "./components/Movies";
 import Navbar from "./components/Navbar";
+import Watchlist from "./components/Watchlist";
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<> <Banner/> <Movies/> </>} />
+          <Route path='/watchlist' element={<Watchlist />} />
+          <Route path='*' element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import movieLogo from "../assets/image.png"
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
         <div className="flex shadow-md space-x-8 items-center pl-3 py-4">
             <img className="w-[50px]" src={movieLogo} alt="movie" />
-            <a href="/" className="text-blue-400 font-bold text-5xl">Movies</a>
-            <a href="/watchlist" className="text-blue-400 font-bold text-5xl">Watchlist</a>
+            <Link to="/" className="text-blue-400 font-bold text-5xl">Movies</Link>
+            <Link to="/watchlist" className="text-blue-400 font-bold text-5xl">Watchlist</Link>
         </div>
     );
 }
