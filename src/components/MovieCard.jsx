af0f400f308904d63272650193139c6605aee3fd
Sherikar Rahul <sherikarrahul59@gmail.com>
2025-07-29 22:09:52 +0530

    Setting API

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function MovieCard() {
+function MovieCard(props) {
+
+  const {title,poster_path} = props.movie
+
   return (
-    <div className='h-[40vh] w-[250px] m-4 rounded-2xl bg-cover bg-center flex items-end hover:scale-110 duration-300 hover:cursor-pointer' style={{backgroundImage : `url(https://images-cdn.ubuy.co.in/6345f652ec87e81aa558cd24-ubuy-online-shopping.jpg)`}}>
-        <div className='w-full text-white text-2xl text-center bg-gray-900/60 rounded-b-2xl p-1'>Joker</div>
+    <div className='h-[40vh] w-[250px] m-4 rounded-2xl bg-cover bg-center flex items-end hover:scale-110 duration-300 hover:cursor-pointer' style={{backgroundImage : `url(https://image.tmdb.org/t/p/original${poster_path})`}}>
+        <div className='w-full text-white text-2xl text-center bg-gray-900/60 rounded-b-2xl p-2'>{title}</div>
     </div>
   )
 }
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState ,useEffect } from 'react'
 import MovieCard from './MovieCard'
+import axios from 'axios'
+import Pagination from './Pagination';
 
 function Movies() {
+
+  const [movies, setMovies] = useState([]);
+  const [pageNo, setPageNo] = useState(1);
+
+  const getApiData = async() => {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=a9fe70b9e2d00af74cda9df5cf32b8e4&language=en-US&page=${pageNo}`);
+      console.log(response.data.results)
+      setMovies(response.data.results)
+    } catch (error) {
+      console.log("Error fetching data from API:", error);
+    }
+  }
+
+  useEffect(() => {
+    getApiData();
+  }, [pageNo])
+
   return (
     <div className='p-5'>
       <div className='text-2xl m-5 font-bold text-center'>Trending Movies</div>
-      <div className='flex flex-row flex-wrap justify-start'><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/></div>
+      <div className='flex flex-row flex-wrap justify-start'>{movies.map((movie,id) => {
+          return <MovieCard key={id} movie={movie} />
+        })}</div>
+      <Pagination pageNo={pageNo} setPageNo={setPageNo}/>
     </div>
   )
 }
 
 export default Movies
+
+
+// https://api.themoviedb.org/3/movie/popular?api_key=a9fe70b9e2d00af74cda9df5cf32b8e4&language=en-US&page=1
