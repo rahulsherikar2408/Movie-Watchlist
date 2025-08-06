c4b202632a0cea3393ad1cc119abcef412cf8ad7
Sherikar Rahul <sherikarrahul59@gmail.com>
2025-08-06 20:41:30 +0530

    Setting Genre Based Filters

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -4,7 +4,17 @@ import genreIds from '../utility/genre';
 function Watchlist({ watchList, setWatchList, handleRemoveFromWatchList }) {
 
   const [search,setSearch] = useState("")
+  const [genreList, setGenreList] = useState(["All Genres"]);
+  const [currGenre, setCurrGenre] = useState("All Genres");
 
+  const handleFilter = (genre) => {
+    setCurrGenre(genre);
+  }
+
+  const handleSearchMovieChange = (e) => {
+    setSearch(e.target.value);
+    setCurrGenre("All Genres");
+  }
 
   const sortIncreasing = () => {
     let sortedIncreasing = watchList.sort((movieA, movieB) => {
@@ -20,17 +30,28 @@ function Watchlist({ watchList, setWatchList, handleRemoveFromWatchList }) {
     setWatchList([...sortedDecreasing])
   }
 
+  useEffect(() => {
+    let temp = watchList.map((movie) => {
+      return genreIds[movie.genre_ids[0]]
+    })
+    temp = new Set(temp)
+    setGenreList(["All Genres",...temp])
+  }, [watchList])
 
   return (
     <>
 
       <div className='flex justify-center flex-wrap m-5'>
-        <div className='flex justify-center items-center mr-4 h-[50px] w-[150px] bg-blue-400 rounded-xl text-white text-xl font-bold hover:bg-blue-400 cursor-pointer duration-200 hover:scale-110'>Action</div>
-        <div className='flex justify-center items-center h-[50px] w-[150px] bg-gray-400/50 rounded-xl text-white text-xl font-bold hover:bg-blue-400 cursor-pointer duration-200 hover:scale-110'>Action</div>
+        {genreList.map((genre) => {
+          return (
+            <div onClick={()=>handleFilter(genre)} className={ (currGenre===genre)?'flex justify-center items-center mr-4 my-2 h-[50px] w-[150px] bg-blue-400 rounded-xl text-white text-xl font-bold hover:bg-blue-400 cursor-pointer duration-200 hover:scale-110' : 'flex justify-center items-center mr-4 my-2 h-[50px] w-[150px] bg-gray-400/50 rounded-xl text-white text-xl font-bold hover:bg-blue-400 cursor-pointer duration-200 hover:scale-110'}>{genre}</div>
+          )
+        })}
+        
       </div>
 
       <div className='flex justify-center my-5'>
-        <input onChange={(e) => setSearch(e.target.value)} value={search} className='bg-gray-200 h-[50px] w-[300px] text-xl px-4 outline-none' placeholder='Search for Movies' type="text" />
+        <input onChange={handleSearchMovieChange} value={search} className='bg-gray-200 h-[50px] w-[300px] text-xl px-4 outline-none' placeholder='Search for Movies' type="text" />
       </div>
 
       <div className='overflow-hidden rounded-lg m-8 border border-gray-200 shadow-md'>
@@ -48,6 +69,8 @@ function Watchlist({ watchList, setWatchList, handleRemoveFromWatchList }) {
             </tr>
           </thead>
           {watchList.filter((movie) => {
+            return (currGenre !== "All Genres")? genreIds[movie.genre_ids[0]] === currGenre : true
+          }).filter((movie) => {
             return movie.title.toLowerCase().includes(search.toLocaleLowerCase())
           }).map((movie) => {
             return (
